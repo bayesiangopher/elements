cae0bd4ec79d656e5991ddf96c9ff259fa5724ac
gtmartem <gtmartem@gmail.com>
2019-06-16 16:37:39 +0300

    [upd] creating dynamic typed array !needs refactoring!

--- ds/array/array.go
+++ ds/array/array.go
@@ -3,15 +3,16 @@
 // 2) array with no duplicates
 
 // O(x):
-// Operation		No duplicates		Duplicates
-// Search			N/2					N
-// Insertion		N, 1				1, 1
-// Deletion			N/2, N/2			N, N/2
+// Operation    No duplicates     Duplicates
+// Search       N/2               N
+// Insertion    N, 1              1, 1
+// Deletion     N/2, N/2          N, N/2
 
 package array
 
 import (
 	"fmt"
+	ref "reflect"
 )
 
 const (
@@ -31,12 +32,18 @@ type PrintOptions struct {
 	offsetEnd   int
 }
 
-// Create creates new array structure filled by values
+type emptyContainerError struct {
+	err string
+}
+
+func (e *emptyContainerError) Error() string {
+	return fmt.Sprintf("creating error: %v", e.err)
+}
+
+// Create creates new array structure filled by values...
 func Create(duplicates bool, values ...interface{}) *Array {
 	array := &Array{duplicates: duplicates}
-	if len(values) > 0 {
-		array.Add(values...)
-	}
+	array.Add(values...)
 	return array
 }
 
@@ -47,12 +54,59 @@ func (a *Array) Add(values ...interface{}) {
 		panic(fmt.Errorf("empty values container for insert: %v",
 			values))
 	}
-	if newSize := a.size + len(values); newSize > cap(a.data) {
-		newCap := int(2 * newSize)
-		newData := make([]interface{}, newCap, newCap)
-		copy(newData, a.data)
-		a.data = newData
+	checkCap(a, a.size+len(values))
+	if len(values) == 1 {
+		a.AddSlice(values[0])
+	} else {
+		a.AddAbstractData(values...)
 	}
+}
+
+// checkCap checks capability of array's data
+func checkCap(array *Array, newSize int) {
+	if newSize > cap(array.data) {
+		array.IncreaseCap(&newSize)
+	}
+	return
+}
+
+// IncreaseCap increases cap of target array
+func (a *Array) IncreaseCap(newSize *int) {
+	newCap := int(2 * *newSize)
+	newData := make([]interface{}, newCap, newCap)
+	copy(newData, a.data)
+	a.data = newData
+}
+
+// AddSlice adds element in array.data with types changes to max bits usage
+func (a *Array) AddSlice(values interface{}) {
+	valuesContainer := ref.ValueOf(values)
+	for i := 0; i < valuesContainer.Len(); i++ {
+		if !a.duplicates {
+			value := valuesContainer.Index(i)
+			if ok := a.checkDuplicates(value); !ok {
+				continue
+			}
+			switch value.Kind() {
+			case ref.Float32, ref.Float64:
+				a.data[a.size] = value.Float()
+				a.size += 1
+			case ref.String:
+				a.data[a.size] = value.String()
+				a.size += 1
+			case ref.Int, ref.Int8, ref.Int16, ref.Int32, ref.Int64:
+				a.data[a.size] = value.Int()
+				a.size += 1
+			case ref.Uint, ref.Uint8, ref.Uint16, ref.Uint32, ref.Uint64:
+				a.data[a.size] = value.Uint()
+				a.size += 1
+			}
+		}
+	}
+}
+
+
+func (a *Array) AddAbstractData(values ...interface{}) {
 	for _, value := range values {
 		if !a.duplicates {
 			if ok := a.checkDuplicates(value); !ok {
@@ -66,9 +120,28 @@ func (a *Array) Add(values ...interface{}) {
 
 // checkDuplicates checks is value in a.data
 func (a *Array) checkDuplicates(value interface{}) (ok bool) {
+	if ref.ValueOf(value).Kind().String() == "slice" {
+		vals := ref.ValueOf(value)
+		for i := 0; i < len(a.data); i++ {
+			if a.data[i] != nil && ref.TypeOf(a.data[i]).Kind().String() == "slice" && ref.ValueOf(a.data[i]).Len() == vals.Len() {
+				maxLen := ref.ValueOf(a.data[i]).Len()
+				for j := 0; j < maxLen; j++  {
+					if vals.Index(j) == ref.ValueOf(a.data[i]).Index(j) {
+						return false
+						}
+					}
+				return true
+				}
+			}
+		}
 	for _, element := range a.data {
-		if value == element {
-			return false
+		if element != nil {
+			if ref.TypeOf(element).Kind().String() == "slice" {
+				continue
+			}
+			if value == element {
+				return false
+			}
 		}
 	}
 	return true
--- ds/array/array_test.go
+++ ds/array/array_test.go
@@ -1,18 +1,39 @@
 package array
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestCreate(t *testing.T) {
-	arr := Create(false, 0,1,2,3,4,5,4,6,7,8,9,3,2,1)
-	arr.Print(PrintOptions{offsetBegin:3,offsetEnd:3})
+	arr := Create(false, 0, 1, 2, 3, 4, 5, 4, 6, 7, 8, 9, 3, 2, 1)
+	arr.Print(PrintOptions{offsetBegin: 3, offsetEnd: 3})
+
+	vecString := []string{"a", "b", "c"}
+	arr.Add(vecString)
+	vecInt := []int16{42, 432, 423}
+	arr.Add(vecInt)
+	vecFloat := []float32{1.1, 1.2, 1.3}
+	arr.Add(vecFloat)
+
+	arr.AddAbstractData([]float64{1,2,3}, []string{"234", "abc"})
+	arr.AddAbstractData([]float64{1,2,3}, []string{"234", "abc"})
+	arr.Add(vecInt)
+
+	for _, element := range arr.data {
+		fmt.Println(reflect.TypeOf(element))
+	}
 }
 
-func TestAdd(t *testing.T) {
-	arr := Create(false, 0,1,2,3,4,5,4,6,7,8,9,3,2,1)
-	arr.Print(PrintOptions{offsetBegin:3,offsetEnd:3})
+//func TestCreatePanic(t *testing.T) {
+//	_ := Create(false, nil)
+//}
 
-	arr.Add(99,98,3,4,5,6)
+func TestAdd(t *testing.T) {
+	arr := Create(false, 0, 1, 2, 3, 4, 5, 4, 6, 7, 8, 9, 3, 2, 1)
+	arr.Print(PrintOptions{offsetBegin: 3, offsetEnd: 3})
+
+	arr.Add(99, 98, 3, 4, 5, 6)
 	arr.Print(PrintOptions{})
 }
